756c34d7bbad92830f5189cd9cd9a510ae0ff899
kubealex <al.rossi87@gmail.com>
2023-05-21 12:26:56 +0200

    update containerfile

--- src/main/java/org/acme/model/Sensor.java
+++ src/main/java/org/acme/model/Sensor.java
@@ -9,10 +9,6 @@ public interface Sensor {
     String deviceId();
     String location();
     Optional<Integer> humidity();
-    // Optional<Integer> minTemp();
-    // Optional<Integer> maxTemp();
-    // Optional<Integer> minHum();
-    // Optional<Integer> maxHum();
     Optional<Boolean> ecoMode();
     Optional<Integer> fanSpeed();
     Optional<Integer> fixedTemperature();
